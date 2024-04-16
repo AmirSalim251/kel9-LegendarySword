69106a3e8804c8724b9b4760ab3bdf0c3e539b41
Dzaky F <dfiantino@gmail.com>
2024-04-16 18:18:04 +0700

    Cuma ngerapiin penempatan asset sama ngebuang asset yg ga kepake di commit sebelumnya
    Ngerapiin penempatan, commitan sebelumnya penempatan asset2nya berantakan bet, susah buat dicari. Sama asset yang ga kepake tapi masuk di folder asset udah diapus

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYER1TURN, PLAYER2TURN, PLAYER3TURN, ENEMY1TURN, ENEMY2TURN, ENEMY3TURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+
+	public GameObject player1Prefab;
+	public GameObject player2Prefab;
+	public GameObject player3Prefab;
+	public GameObject enemy1Prefab;
+	public GameObject enemy2Prefab;
+	public GameObject enemy3Prefab;
+
+	Unit player1Unit;
+	Unit player2Unit;
+	Unit player3Unit;
+
+	Unit enemy1Unit;
+	Unit enemy2Unit;
+	Unit enemy3Unit;
+
+	Unit activePlayer;
+	Unit activeEnemy;
+
+	Unit targetedPlayer;
+	Unit targetedEnemy;
+
+	public Text dialogueText;
+
+	public BattleHUD playerHUD;
+	public BattleHUD enemyHUD;
+
+	public BattleState state;
+
+	bool isActionAllowed;
+
+    void Start()
+    {
+		state = BattleState.START;
+		StartCoroutine(SetupBattle());
+    }
+
+	IEnumerator SetupBattle()
+	{
+		player1Unit = player1Prefab.GetComponent<Unit>();
+		player2Unit = player2Prefab.GetComponent<Unit>();
+		player3Unit = player3Prefab.GetComponent<Unit>();
+
+		activePlayer = player1Unit;
+		activeEnemy = enemy1Unit;
+
+		enemy1Unit = enemy1Prefab.GetComponent<Unit>();
+
+		// dialogueText.text = enemy1Unit.unitName + " approaches...";
+
+		playerHUD.SetHUD(activePlayer);
+		enemyHUD.SetHUD(enemy1Unit);
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.PLAYER1TURN;
+		PlayerTurn();
+	}
+
+	IEnumerator PlayerAttack()
+	{
+		isActionAllowed = false;
+		bool isDead = enemy1Unit.TakeDamage(activePlayer.attack);
+
+		dialogueText.text = activePlayer.name + " used Attack";
+		yield return new WaitForSeconds(2f);
+		enemyHUD.SetHP(enemy1Unit.currentHP);
+		dialogueText.text = "The attack is successful!";
+
+		yield return new WaitForSeconds(2f);
+
+		if(isDead)
+		{
+			state = BattleState.WON;
+			EndBattle();
+		} 
+		else
+		{
+			if (state == BattleState.PLAYER1TURN) 
+			{
+				state = BattleState.PLAYER2TURN;
+				activePlayer = player2Unit;
+				PlayerTurn();
+			}
+			else if (state == BattleState.PLAYER2TURN) 
+			{
+				state = BattleState.PLAYER3TURN;
+				activePlayer = player3Unit;
+				PlayerTurn();
+			}
+			else
+			{
+				state = BattleState.ENEMY1TURN;
+				activePlayer = player1Unit;
+				playerHUD.SetHUD(activePlayer);
+				StartCoroutine(EnemyTurn());
+			}
+		}
+	}
+
+	IEnumerator EnemyTurn()
+	{
+		int randomTarget = Random.Range(1, 3);
+
+		if (randomTarget == 1)
+		{
+			targetedPlayer = player1Unit;
+		}
+		else if (randomTarget == 2)
+		{
+			targetedPlayer = player2Unit;
+		}
+		else if (randomTarget == 3)
+		{
+			targetedPlayer = player3Unit;
+		}
+
+		dialogueText.text = activeEnemy.unitName + " used Attack on " + targetedPlayer.unitName;
+
+		yield return new WaitForSeconds(1f);
+
+		bool isDead = activePlayer.TakeDamage(enemy1Unit.attack);
+
+		playerHUD.SetHP(activePlayer.currentHP);
+
+		yield return new WaitForSeconds(1f);
+
+		if(isDead)
+		{
+			state = BattleState.LOST;
+			EndBattle();
+		} 
+		else
+		{
+			state = BattleState.PLAYER1TURN;
+			PlayerTurn();
+		}
+	}
+
+	void EndBattle()
+	{
+		if(state == BattleState.WON)
+		{
+			dialogueText.text = "You won the battle!";
+		} 
+		else if (state == BattleState.LOST)
+		{
+			dialogueText.text = "You were defeated.";
+		}
+	}
+
+	void PlayerTurn()
+	{
+		isActionAllowed = true;
+		playerHUD.SetHUD(activePlayer);
+		dialogueText.text = "Choose an action:";
+	}
+
+	IEnumerator PlayerHeal()
+	{
+		state = BattleState.ENEMY1TURN;
+		activePlayer.Heal(5);
+
+		playerHUD.SetHP(activePlayer.currentHP);
+		dialogueText.text = "You feel renewed strength!";
+
+		yield return new WaitForSeconds(2f);
+
+		StartCoroutine(EnemyTurn());
+	}
+
+	public void OnAttackButton()
+	{
+		if (!isActionAllowed)
+			return;
+
+		StartCoroutine(PlayerAttack());
+	}
+
+	public void OnDefendButton()
+	{
+		if (state != BattleState.PLAYER1TURN)
+			return;
+
+		StartCoroutine(PlayerHeal());
+	}
+
+	public void OnSkill1Button()
+	{
+		if (activePlayer.unitName == "Alex")
+		{
+			
+		}
+	}
+
+	public void OnSkill2Button()
+	{
+		
+	}
+
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+	public string unitName;
+    
+    public int maxHP;
+    public int maxMP;
+    public int attack;
+    public int defense;
+	public int currentHP;
+	public int currentMP;
+
+    public UnitStats alex = new UnitStats("Alex");
+
+	void Start ()
+	{
+		setStats();	
+	}
+
+	public void setStats() 
+    {
+        if (unitName == "Alex")
+        {
+            maxHP = 150;
+            maxMP = 5;
+            attack = 25;
+            defense = 20;
+        }
+        else if (unitName == "Freya")
+        {
+            maxHP = 120;
+            maxMP = 6;
+            attack = 18;
+            defense = 15;
+        }
+        else if (unitName == "Magnus")
+        {
+            maxHP = 130;
+            maxMP = 6;
+            attack = 15;
+            defense = 25;
+        }
+		else if (unitName == "Mob Minion")
+		{
+			maxHP = 70;
+            attack = 15;
+            defense = 15;
+		}
+		else if (unitName == "Mob Boss")
+		{
+			maxHP = 350;
+            attack = 25;
+            defense = 30;
+		}
+        
+        currentHP = maxHP;
+        currentMP = maxMP;
+    }
+
+	public bool TakeDamage(int dmg)
+	{
+		currentHP -= dmg;
+
+		if (currentHP <= 0)
+			return true;
+		else
+			return false;
+	}
+
+	public void Heal(int amount)
+	{
+		currentHP += amount;
+		if (currentHP > maxHP)
+			currentHP = maxHP;
+	}
+
+}
