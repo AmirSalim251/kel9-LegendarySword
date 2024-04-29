a3c6810a148e1557ce93a51c6b6bd7deba54a4b6
Fahmi Ihsan <7c.fahmi@gmail.com>
2024-04-29 17:58:55 +0700

    integrate qte echanics

--- Assets/Scripts/Controller_Battle.cs
+++ Assets/Scripts/Controller_Battle.cs
@@ -25,6 +25,7 @@ public class Controller_Battle : MonoBehaviour
 
     Controller_CharData ActiveTarget;
 
+
     public static Controller_CharData player1Unit;
     public static Controller_CharData player2Unit;
     public static Controller_CharData player3Unit;
@@ -34,6 +35,8 @@ public class Controller_Battle : MonoBehaviour
     public static Controller_EnemyData enemyUnit;
 
     public BattleState state;
+
+    [SerializeField] public Image qteIndicator;
 
     bool isActionAllowed;
 
@@ -71,6 +74,51 @@ public class Controller_Battle : MonoBehaviour
         turnCount = 0;
         state = BattleState.PLAYERTURN;
         PlayerTurn();
+    }
+
+    IEnumerator QTEEvent()
+    {
+        Log.text = "Press 'A' key repeatedly to attack!";
+        float timeLimit = 3f; // Adjust the time limit as desired
+        float currentTime = 0f;
+        float fillAmount = 0f;
+        float decreaseRate = 0.2f;
+        float increaseRate = 0.1f;
+
+        while (currentTime < timeLimit)
+        {
+            currentTime += Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                fillAmount += increaseRate;
+                qteIndicator.fillAmount = fillAmount;
+            }
+            else
+            {
+                fillAmount -= decreaseRate * Time.deltaTime;
+            }
+
+            fillAmount = Mathf.Clamp01(fillAmount);
+            qteIndicator.fillAmount = fillAmount;
+
+            if (fillAmount >= 1f)
+            {
+                Log.text = "Attack successful!";
+                StartCoroutine(HeroAttack());
+                qteIndicator.fillAmount = 0f;
+                break;
+            }
+
+            yield return null;
+        }
+
+        if (fillAmount < 1f)
+        {
+            Log.text = "Attack missed!";
+            qteIndicator.fillAmount = 0f;
+            PassTurn();
+        }
     }
 
     IEnumerator HeroAttack()
@@ -385,8 +433,15 @@ public class Controller_Battle : MonoBehaviour
     {
         if (!isActionAllowed)
             return;
-
-        StartCoroutine(HeroAttack());
+        if (state == BattleState.PLAYERTURN)
+        {
+            StartCoroutine(QTEEvent());
+        }
+        else
+        {
+            StartCoroutine(HeroAttack());
+        }
+        
     }
 
     public void OnBlockButton()
