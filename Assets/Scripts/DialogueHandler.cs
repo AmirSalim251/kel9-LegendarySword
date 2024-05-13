c13e306c5b8471979bc973839a5f885612aa35ea
Dzaky F <dfiantino@gmail.com>
2024-05-13 21:28:34 +0700

    Update Progress Scene VN
    Ini udah bisa dipakek sih walau belum lengkap, sistemnya udah jalan aman sih harusnya. Ini dibikin popup ama animasi yang kaya di persona, buat troll panel masi belom, ama masih menunggu sprite lainnya kaya si pedang, enemy(gatau sih perlu ato ngga ditaruh sprite nya pas si wolfnya lagi ngomong raurr gitu) ama ekspresi2 karakter

--- Assets/Scripts/DialogueHandler.cs
+++ Assets/Scripts/DialogueHandler.cs
@@ -2,28 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DialogueHandler : MonoBehaviour
 {
+    public TextAsset textFile;
+    public float changeSpeakerDelay;
+
     public TextMeshProUGUI speaker;
     public TextMeshProUGUI sentence;
-    public string[] lines;
-    public float textSpeed = 0.05f;
 
-    public TextAsset textFile;
+    public Animator dialogue;
+
+    public GameObject continueText;
+
+    public Animator Alex;
+    public Animator Freya;
+    public Animator Magnus;
+
+    public string[] lines;
 
     List<string> speakers = new List<string>();
     List<string> sentences = new List<string>();
     
     private int index = 0;
+    private bool changingSpeaker = false;
 
     void Start() 
     {
         speaker.text = string.Empty;
         sentence.text = string.Empty;
+        continueText.SetActive(true);
 
         ReadFile();
-        StartCoroutine(TypeLines());
+
+        SetCharacterImage(speakers[index], true);
+
+        speaker.text = speakers[index];
+        sentence.text = sentences[index];
+    }
+
+    void Update() 
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+           if (changingSpeaker) ClickedWhenChangingSpeaker();
+           else NextLine();
+        }
     }
 
     void ReadFile() {
@@ -48,45 +73,84 @@ public class DialogueHandler : MonoBehaviour
         }
     }
 
-    void Update() 
-    {
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            if (sentence.text == sentences[index]) 
-            {
-                NextLine();
-            }
-            else 
-            {
-                StopAllCoroutines();
-                sentence.text = sentences[index];
-            }
-        }
-    }
-
-    IEnumerator TypeLines() 
-    {
-        speaker.text = speakers[index];
-
-        foreach (char letter in sentences[index].ToCharArray()) 
-        {
-            sentence.text += letter;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
     void NextLine() 
-    {
-        if (index < lines.Length - 1) 
+    {        
+        index++;
+
+        if (index >= lines.Length) 
         {
-            index++;
-            speaker.text = string.Empty;
-            sentence.text = string.Empty;
-            StartCoroutine(TypeLines());
-        }   
+            if (index == lines.Length) StartCoroutine(EndDialogue());
+            return;
+        }
+
+        if (speakers[index] != speakers[index-1]) StartCoroutine(ChangeSpeaker());
         else 
         {
-            gameObject.SetActive(false);
+            dialogue.SetTrigger("Next Line");
+            speaker.text = speakers[index];
+            sentence.text = sentences[index];
         }
     }   
+
+    void SetCharacterImage(string speaker, bool activate)
+    {
+        if (speaker == "Alex") 
+        {
+            if (activate == true) Alex.SetTrigger("Entry");
+            else Alex.SetTrigger("Exit");
+        }
+        else if (speaker == "Freya")
+        {
+            if (activate == true) Freya.SetTrigger("Entry");
+            else Freya.SetTrigger("Exit");
+        }
+        else if (speaker == "Magnus")
+        {
+            if (activate == true) Magnus.SetTrigger("Entry");
+            else Magnus.SetTrigger("Exit");
+        }
+    }
+
+    IEnumerator ChangeSpeaker()
+    {
+        changingSpeaker = true;
+
+        SetCharacterImage(speakers[index-1], false);
+        dialogue.SetTrigger("Exit");
+        continueText.SetActive(false);
+
+        yield return new WaitForSeconds(changeSpeakerDelay);
+
+        speaker.text = speakers[index];
+        sentence.text = sentences[index];
+
+        SetCharacterImage(speakers[index], true);
+        dialogue.SetTrigger("Entry");
+        continueText.SetActive(true);
+
+        changingSpeaker = false;
+    }
+
+    IEnumerator EndDialogue()
+    {
+        SetCharacterImage(speakers[index-1], false);
+        dialogue.SetTrigger("Exit");
+
+        yield return new WaitForSeconds(changeSpeakerDelay);
+
+        Destroy(gameObject);
+    }
+
+    void ClickedWhenChangingSpeaker()
+    {
+        StopAllCoroutines();
+        changingSpeaker = false;
+
+        speaker.text = speakers[index];
+        sentence.text = sentences[index];
+
+        SetCharacterImage(speakers[index], true);
+        dialogue.SetTrigger("Entry");
+        continueText.SetActive(true);
+    }
 }
